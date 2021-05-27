f45b60863f9c8059d83d4d437cf16826ade33915
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-05-27 23:08:21 +0900

    preparePt instead of stepk.

--- surface.cc
+++ surface.cc
@@ -19,8 +19,9 @@ int main(int argc, const char* argv[]) {
     std::stringstream ins(s);
     v.emplace_back(num_t(0));
     ins >> v[v.size() - 1];
+    if(4 * 4 + 4 * 4 <= v.size()) break;
   }
-  assert(v.size() == 4 * 4 + 4 * 4 + 1);
+  assert(v.size() == 4 * 4 + 4 * 4);
   SimpleMatrix<num_t> invA(4, 4);
   SimpleMatrix<num_t> invR(4, 4);
   for(int i = 0; i < invA.rows(); i ++)
@@ -29,7 +30,7 @@ int main(int argc, const char* argv[]) {
   for(int i = 0; i < invR.rows(); i ++)
     for(int j = 0; j < invR.cols(); j ++)
       invR(i, j) = v[i * invR.cols() + j + invA.rows() * invA.cols()];
-  std::cout << stepk<num_t>(invA, SimpleMatrix<num_t>(), invR, SimpleMatrix<num_t>(), int(v[v.size() - 1]), SimpleVector<num_t>(3).ek(0)) << std::endl;
+  std::cout << preparePt<num_t>(SimpleMatrix<num_t>(), invR, SimpleMatrix<num_t>(), SimpleVector<num_t>(3).ek(0)) << std::endl;
   return 0;
 }
 
--- surface.hh
+++ surface.hh
@@ -1,13 +1,9 @@
 #if !defined(_SURFACE_)
 
-template <typename T> SimpleVector<T> stepk(const SimpleMatrix<T>& invarF, const SimpleMatrix<T>& implInvarF, const SimpleMatrix<T>& invarR, const SimpleMatrix<T>& implInvarR, const int& k, const SimpleVector<T>& inv) {
-  std::vector<SimpleMatrix<T> > AinvarF;
-  AinvarF.emplace_back(invarF);
-  for(int i = 1; i < k; i ++)
-    AinvarF.emplace_back(AinvarF[AinvarF.size() - 1] * AinvarF[AinvarF.size() - 1]);
-  SimpleVector<T> last(invarF.cols());
+template <typename T> SimpleVector<T> preparePt(const SimpleMatrix<T>& implInvarF, const SimpleMatrix<T>& invarR, const SimpleMatrix<T>& implInvarR, const SimpleVector<T>& in) {
+  SimpleVector<T> last(invarR.rows() + implInvarF.rows());
   SimpleVector<T> regionv(invarR.cols());
-  regionv.setVector(0, inv).setVector(inv.size(), implInvarR * inv);
+  regionv.setVector(0, in).setVector(in.size(), implInvarR * in);
   auto work(regionv);
   for(int i = 0; i < work.size(); i ++)
     work[i] = tan(work[i]);
